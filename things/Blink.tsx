eccab8a70a6cad8af7482344ab2d16b8e2d1bcb1
Ben Foxall <benfoxall@gmail.com>
2020-07-16 16:14:07 +0100

    add the hacks

---
+++ things/Blink.tsx
@@ -0,0 +1,31 @@
+const Name = 'Blink'
+const Summary = 'This is a thing that blinks on and off'
+
+const Config = {
+  interval: 'number(0, 200)'
+}
+
+
+
+
+
+
+
+
+
+const Blink = () => 
+  <Thing>
+    <Name>
+      Blink
+    </Name>
+    <Summary>
+      This is a thing that blinks on and off
+    </Summary>
+    
+    <Script>
+
+    </Script>
+
+
+  </Thing>
+
